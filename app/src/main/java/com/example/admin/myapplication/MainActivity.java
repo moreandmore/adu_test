fb50c3b452f094b502002b74b04146b4cf7bbe69
junbiao.du <junbiao.du@cutt.com>
2016-05-24 16:50:45 +0800

    commit -a

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -11,6 +11,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//       dev master   change   merge
+//       dev master   change newwww  merge
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // change value       newwwwww
     }
 }
